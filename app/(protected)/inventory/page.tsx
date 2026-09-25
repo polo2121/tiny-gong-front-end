67ac6cf87c61ce6361517f03a44035dcc4df40c1
polo2121 <72909938+polo2121@users.noreply.github.com>
2026-09-25 12:58:25 +0700

    Add isolated inventory section

---
+++ app/(protected)/inventory/page.tsx
@@ -0,0 +1,10 @@
+import ProtectedPageTitle from "@/components/PageHeader";
+
+export default function InventoryPage() {
+  return (
+    <ProtectedPageTitle
+      title="Inventory"
+      subtitle="ပစ္စည်း စီမံခန့်ခွဲ"
+    />
+  );
+}
