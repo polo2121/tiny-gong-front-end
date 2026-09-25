9ba3bd4f22e08eaf6275d808bad233322b0d3b14
polo2121 <72909938+polo2121@users.noreply.github.com>
2026-09-25 12:58:26 +0700

    Add isolated products section

---
+++ app/(protected)/products/page.tsx
@@ -0,0 +1,10 @@
+import ProtectedPageTitle from "@/components/PageHeader";
+
+export default function ProductsPage() {
+  return (
+    <ProtectedPageTitle
+      title="Products"
+      subtitle="ပစ္စည်းလက်ကျန်"
+    />
+  );
+}
